65dbaed90f82a4718abd2af0888effcad0c0facd
johannes karoff <johannes@karoff.net>
2020-09-03 19:20:42 +0200

    wip

--- web-client/src/main/scala/web/client/HttpClient.scala
+++ web-client/src/main/scala/web/client/HttpClient.scala
@@ -11,7 +11,7 @@ import java.nio.ByteBuffer
 import boopickle.Default._
 import chameleon.ext.boopickle._
 
-object WebClient {
+object HttpClient {
   private val client = Client[ByteBuffer, ApiResult, ApiError](WebTransport)
 
   val api = client.wire[Api[ApiResult]]
